2cfc1bdf66fdd4e59ae634b38920e8f512bf981a
tsaichiehhuang <a0970605512@gmail.com>
2023-07-20 14:22:45 +0800

    fix(w3p5):重新整理頁面時，即時顯示的愛心會消失

--- workspace/kelly/CanChu/pages/posts/[id].jsx
+++ workspace/kelly/CanChu/pages/posts/[id].jsx
@@ -6,7 +6,7 @@ import Cookies from 'js-cookie'
 const apiUrl = process.env.API_DOMAIN
 
 export async function getServerSideProps(context) {
-  const { req, res, params } = context
+  const { req, res } = context
   const accessToken = req.cookies.accessToken
 
   // 如果未登入，重定向到登入頁面
@@ -24,9 +24,13 @@ export async function getServerSideProps(context) {
 export default function Demo() {
   const postId = Cookies.get('postId')
   const accessToken = Cookies.get('accessToken')
-  // eslint-disable-next-line consistent-return
-  const [postData, setPostData] = useState([]) // 改為空數組作為初始值
+  const [postData, setPostData] = useState({
+    data: {},
+    liked: false,
+    likeCount: 0
+  })
 
+  //fix:重新整理頁面時，即時顯示的愛心會消失
   useEffect(() => {
     const fetchPostData = async () => {
       try {
@@ -40,7 +44,12 @@ export default function Demo() {
 
         if (response.ok) {
           const data = await response.json()
-          setPostData(data?.data?.post || [])
+          const post = data?.data?.post || {}
+          setPostData({
+            data: post,
+            liked: post.is_liked || post.is_like || false,
+            likeCount: post.like_count || 0
+          })
         } else {
           console.error('獲取貼文數據時出錯')
         }
@@ -49,16 +58,21 @@ export default function Demo() {
       }
     }
     fetchPostData()
-  }, [])
+  }, [postId])
+  const { data, liked, likeCount } = postData
+  const formattedLikeCount = likeCount === 0 ? 0 : likeCount
   return (
     <div>
       <Header />
       <Post
-        data={postData}
+        data={data}
         showComments={true}
         showImage={true}
         showEditIcon={false}
         enableClick={false}
+        liked={liked} // 將愛心狀態傳遞給 Post 元件
+        likeCount={likeCount} // 將愛心數量傳遞給 Post 元件
+        formattedLikeCount={formattedLikeCount} // 將格式化後的愛心數量傳遞給 Post 元件
       />
     </div>
   )
