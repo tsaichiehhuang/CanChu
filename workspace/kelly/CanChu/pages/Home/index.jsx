a013b54aee0a090693d9a080fdcaaa674a5407ff
tsaichiehhuang <a0970605512@gmail.com>
2023-07-26 10:36:19 +0800

    好友列表

--- workspace/kelly/CanChu/pages/Home/index.jsx
+++ workspace/kelly/CanChu/pages/Home/index.jsx
@@ -10,6 +10,7 @@ import useFetchUserProfile from '@/hook/userFetchUserProfile'
 import useFriendsPending from '@/hook/useFriendsPending'
 import Cookies from 'js-cookie'
 import IsPictureUrlOk from '@/components/IsPictureUrlOk'
+import Link from 'next/link'
 
 export default function Home() {
   const postData = useFetchPostsData()
@@ -62,27 +63,13 @@ export default function Home() {
     </div>
   )
   const friendList = () => {
-    const renderFriendSection = (icon, text) => (
+    const renderFriendSection = (img, text) => (
       <div className={styles.friendListSection}>
-        {icon ? (
-          <img
-            style={{ marginLeft: '1%', width: '28px', borderRadius: '50%' }}
-            src={icon}
-          />
-        ) : (
-          <div className={styles.friendListIcon}></div>
-        )}
-        <div>{text}</div>
-      </div>
-    )
-    const renderIconSection = (icon, text) => (
-      <div className={styles.friendListSection}>
-        {icon ? (
-          <img style={{ marginLeft: '1%', width: '10%' }} src={icon} />
-        ) : (
-          <div className={styles.friendListIcon}></div>
-        )}
-        <div style={{ color: '#767676', fontWeight: '700' }}>{text}</div>
+        <img
+          style={{ marginLeft: '1%', width: '40px', borderRadius: '50%' }}
+          src={img}
+        />
+        <div className={styles.friendRequestText}>{text}</div>
       </div>
     )
     return (
@@ -100,17 +87,48 @@ export default function Home() {
             margin: '3% 0'
           }}
         ></div>
-        {renderIconSection('/friends.png', '我的好友')}
+
+        <div className={styles.friendListSection}>
+          <div style={{ margin: '0% 3.5%', width: '10%' }}>
+            <img style={{ width: '100%' }} src='/friends.png' />
+          </div>
+          <div
+            style={{ color: '#767676' }}
+            className={styles.friendRequestText}
+          >
+            我的好友
+          </div>
+        </div>
         <div className={styles.friendListMyFriend}>
-          {/* {friends.map((friend, index) => (
-            <div className={styles.friendListSection} key={index}>
-              <div className={styles.friendListIcon}></div>
-              <div>{friend}</div>
+          {friends.map((friend, index) => (
+            <div key={index}>
+              <Link
+                href='/users/[user.id]'
+                as={`/users/${friend.id}`}
+                className={styles.friendListSection}
+                prefetch
+              >
+                <IsPictureUrlOk
+                  className={styles.friendRequestImg}
+                  userState={friend}
+                />
+                <div className={styles.friendRequestText}>{friend.name}</div>
+              </Link>
             </div>
-          ))} */}
+          ))}
           {friendsPending.map((friend) => renderFriendRequest(friend))}
         </div>
-        {renderIconSection('/options.png', '查看全部')}
+
+        <div className={styles.friendListSection}>
+          <img style={{ margin: '0% 1.5%', width: '15%' }} src='/options.png' />
+
+          <div
+            style={{ color: '#767676' }}
+            className={styles.friendRequestText}
+          >
+            查看全部
+          </div>
+        </div>
       </div>
     )
   }
